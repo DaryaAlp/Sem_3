d5963ef3199062d24e18d5ae80dbce2982d4f321
Darya <d.a_alpatskaya@mail.ru>
2023-03-30 14:50:07 +0300

    Добавили задачи 17,18,20,22

---
+++ 22/Program.cs
@@ -0,0 +1,22 @@
+﻿// 22 Напишите программу, которая принимает на вход число (N)
+//  и выдаёт таблицу квадратов чисел от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+
+// 2 -> 1,4
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int N = new int[N];
+// n=5
+// 0 1 2 3 4 
+// 0 0 0 0 0
+//1 2 3 4 5
+//1 4 9 16 25
+ for (int i = 1; i <= N; i++)
+    result[i - 1] = i * i;
+   
+    for (int i = 0; i < N; i++)
+Console.Write($"{result[i]} ");
+    for (int i = 0; i < N; i++)
+Console.Write($"{result[i]} ");
